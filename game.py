602b2e69b512ee9157c812259639c530379f0997
Damian Bicocchi <Damian.bicocchi211148@alumnos.info.unlp.edu.ar>
2023-03-19 14:57:44 -0300

    Agregado el contador de correctas

--- game.py
+++ game.py
@@ -7,6 +7,8 @@ times = 5
 # Contador inicial de tiempo.
 # Esto toma la fecha y hora actual.
 init_time = datetime.now()
+#Contador de correctas
+cont = 0
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
 for i in range(0, times):
     correcto = False
@@ -16,6 +18,8 @@ for i in range(0, times):
     operator = choice(operators)
     # Se imprime la cuenta.
     print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    # Le pedimos al usuario el resultado
+    result = input("resultado: ")
     match operator:
         case "+":
             correcto = (result == (number_1 + number_2))
@@ -25,9 +29,14 @@ for i in range(0, times):
             correcto = (result == (number_1 * number_2))
         case "//":
             correcto = (result == (number_1 // number_2))
-        
-# Le pedimos al usuario el resultado
-    result = input("resultado: ")
+    if correcto:
+        print("Muy bien :)")
+        cont += 1
+    else:
+        print("Ups, incorrecto :(")
+      
+
+    
 # Al terminar toda la cantidad de cuentas por resolver.
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()
@@ -35,3 +44,4 @@ end_time = datetime.now()
 total_time = end_time - init_time
 # Mostramos ese tiempo en segundos.
 print(f"\n Tardaste {total_time.seconds} segundos.")
+
